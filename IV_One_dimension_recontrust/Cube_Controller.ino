91420688c7169e9137c890b6a6538a4024332ae1
Greg Di ZHANG <Freg_9m@foxmail.com>
2016-12-05 21:59:44 +0800

    PID built

---
+++ IV_One_dimension_recontrust/Cube_Controller.ino
@@ -0,0 +1,90 @@
+#include "Cube_Controller.h"
+//#define LQR_Type
+#define PID_Type
+
+#define Angle_offset -0.12
+#define Angle_dot_offset -0.11011
+#define Trust_Value 0.1
+
+float State_now[3];              // State used in Controller
+float State_bar[3];              // State measured from IMU and Ecoder;
+float State_prev[3];             // State used in Controller
+float Voltage_now;
+int First_Flag=0;
+float Angle_final;
+float Angle_final_prev;
+#ifdef  LQR_Type
+    /* Low Pass Filter */
+    float State_LPF, State_LPF_Next=0;// Apply LPF in Tilt Angle
+    #define  Alpha_LPF 0.02             // 0 Means NO Filter, Typical Value could be 0.02.
+    /* LQR Control */
+    float Kd[3];
+    float u_now;
+#endif
+
+#ifdef  PID_Type
+    float Kp,Kd;
+#endif
+
+void Cube_Controller_SetUp ( void )
+{
+    #ifdef  LQR_Type
+        Kd[0] = -182.7363;
+        Kd[1] = -18.0346;
+        Kd[2] = -0.6265;
+    #endif
+    #ifdef  PID_Type
+        Kp = 300;
+        Kd = 5000;
+    #endif
+}
+
+int Cube_Controller( float body_angle, float body_angle_dot, float wheel_angle_dot )
+{
+    float Angle_gyro=0;
+    State_bar[0] = body_angle - Angle_offset;
+    State_bar[1] = body_angle_dot - Angle_dot_offset;
+    State_bar[2] = wheel_angle_dot;
+    //Filter
+    if (First_Flag ==0 )
+    {
+        Angle_final = State_bar[0];
+        First_Flag = 1;
+    }
+    Angle_gyro = Angle_final - State_bar[1]*0.001;
+    Angle_final = State_bar[0] * Trust_Value + Angle_gyro * (1 - Trust_Value);
+
+
+    #ifdef  LQR_Type
+        //Low Pass Filter Get Value
+        State_LPF = State_LPF_Next;
+        //Set State_now
+        State_now[0] = State_bar[0] - State_LPF - Angle_offset;//Low Pass Filter
+        State_now[1] = State_bar[1];
+        State_now[2] = State_bar[2];
+        //Implement of LQR
+        u_now = Kd[0]*State_now[0] + Kd[1]*State_now[1] + Kd[2]*State_now[2];
+        /*if (u_now >= 0)
+        {
+        Voltage_now = (u_now*1.419 +4)*(u_now*1.419 +4);
+        }
+        else
+        {
+        Voltage_now = -(u_now*1.419 -4)*(u_now*1.419 -4);
+        }*/
+        //Low Pass Filter Updata
+        State_LPF_Next = (1- Alpha_LPF)*State_LPF + Alpha_LPF*State_bar[0];
+    #endif
+    #ifdef  PID_Type
+       // Voltage_now = Kp * (-State_bar[0] + Angle_offset) + Kd * (State_bar[1]- Angle_dot_offset);
+       Voltage_now = Kp * (-Angle_final) + Kd * (-Angle_final +Angle_final_prev);
+    #endif
+    State_prev[0] = State_bar[0];
+    State_prev[1] = State_bar[1];
+    State_prev[2] = State_bar[2];
+    Angle_final_prev = Angle_final;
+    if (Voltage_now >= 255) { Voltage_now = 255; }
+    if (Voltage_now <= -255) { Voltage_now = -255; }
+    return (int)Voltage_now;
+}
+
---
+++ IV_One_dimension_recontrust/II_One_dimension_recontrust.ino
@@ -0,0 +1,180 @@
+#include <Arduino.h>
+#include <math.h>
+#include <Wire.h>
+#include <String.h>
+#include "pinout.h"
+#include "MPU6050.h"
+#include "helper_3dmath.h"
+#include "maxon.h"
+#include "Cube_Controller.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define DEBUG false
+
+// Maxon DEC 50/5 motor
+Maxon maxon;
+
+MPU6050 mpu1;
+MPU6050 mpu2(0x69);
+
+int16_t ax1, ay1, az1, gx1, gy1, gz1;
+int16_t ax2, ay2, az2, gx2, gy2, gz2;
+
+float body_angle, body_angle_dot;
+float body_angle_LPF = 0;
+float wheel_angle_dot;
+float input_current;
+const float alpha_body_angle = 0.3;
+
+unsigned volatile int countTimer = 0;
+unsigned volatile int countHz = 0;
+
+ISR(TIMER4_OVF_vect)
+{
+  countHz = countTimer;
+  countTimer = 0;
+  TCNT4 = 0xB1DF;
+}
+
+// Capture the rising edge
+ISR(TIMER4_CAPT_vect)
+{
+  countTimer++;
+}
+
+void setup()
+{
+	// Join I2C bus, open up serial to computer
+	Wire.begin();
+	Serial.begin(115200);
+	while (!Serial.available()) ;
+
+	// Initialize MPU, attach interrupt to pin 2
+	// Initialize two MPU in the same buss
+	mpu1.initialize();
+	mpu2.initialize();
+
+	// verify connection
+//	Serial.println("Testing device connections...");
+//	Serial.println(mpu1.testConnection() ? "MPU6050 1 connection successful" : "MPU6050 1 connection failed");
+//	Serial.println(mpu2.testConnection() ? "MPU6050 2 connection successful" : "MPU6050 2 connection failed");
+
+	// Calibrate both MPU-6050 in acelX acelY acelZ gyroX gyroY gyroZ
+	setMPUofffset(mpu1, -2761,  -1161, 864, 91,  -2,  -7); //
+	setMPUofffset(mpu2, -2743,  -283,  747, 42,  -32, 24);//
+
+	// Initialize the motor, Nidec and Maxon;
+	maxon.begin(P_MAXON_IN1, P_MAXON_IN2, P_MAXON_DIR, P_MAXON_EN, P_MAXON_SPEED, P_MAXON_READY, P_MAXON_FEEDBACK, P_MAXON_STATUS);
+	maxon.setMode(SPEED_MODE_OPEN);
+  maxon.enable();
+
+	// TODO3: Test the servo
+
+	// TODO4: Test the encoder
+    //Serial1.begin(38400);
+
+	// Final TODO5: implement the LQR controller, online or offline
+	Cube_Controller_SetUp();
+
+  // MPU initial update
+  mpu1.getMotion6(&ax1, &ay1, &az1, &gx1, &gy1, &gz1);
+  mpu2.getMotion6(&ax2, &ay2, &az2, &gx2, &gy2, &gz2);
+  
+  // Debugging timer
+  pinMode(P_MAXON_STATUS, OUTPUT);
+  digitalWrite(P_MAXON_STATUS, LOW);
+
+	// Set up the timer
+//	Timer1.initialize(100000); // set a timer of length 100000 microseconds (or 0.1 sec - or 10Hz => the led will blink 5 times, 5 cycles of on-and-off, per second)
+//	Timer1.attachInterrupt( timerIsr ); // attach the service routine here
+  pinMode(ICP4, INPUT);
+  pinMode(7, OUTPUT);
+
+  // initialize Timer4
+  cli();         // disable global interrupts
+  TCCR1A = 0;    // set entire TCCR1A register to 0
+  TCCR1B = 0;    // set entire TCCR1B register to 0 
+  
+  // count from 45535, for 16-bit counter, 45535 to 65535 get 20000 count
+  // minus 1480 more to compensate the calculation time, 0.74 ms
+  TCNT1 = 0xB7A7;
+  // Set CS10 bit so timer runs at clock speed: 100Hz, 0.01 sec
+  // Prescaling: clk/8, CS11 set to 1
+  TCCR1B |= (1 << CS11);  // Similar: TCCR1B |= _BV(CS11);
+
+  // and input capture interrupt enable
+  TIMSK1 |= (1 << TOIE1); 
+  
+  TCCR4A = 0;    // set entire TCCR1A register to 0
+  TCCR4B = 0;    // set entire TCCR1B register to 0 
+  
+  ICR4H = 0;
+  ICR4L = 0;
+
+  // count from 45535, for 16-bit counter, 45535 to 65535 get 20000 count
+  TCNT4 = 0xB1DF;
+  
+  // Set CS10 bit so timer runs at clock speed: 100Hz, 0.01 sec
+  // Prescaling: clk/8, CS11 set to 1
+  TCCR4B |= (1 << CS41);  // Similar: TCCR1B |= _BV(CS11);
+//  TCCR4B |= (1 << CS42);  // Similar: TCCR1B |= _BV(CS11);
+  
+  // ICES1 Input Capture Edge Select flag, 1 for rising, 0 for falling
+  TCCR4B |= (1 << ICES4);
+  
+  // and input capture interrupt enable
+  TIMSK4 |= (1 << ICIE4); 
+  // enable Timer overflow interrupt:
+  TIMSK4 |= (1 << TOIE4); 
+  
+  // enable global interrupts:
+  sei();
+  delay(1000);
+}
+
+void loop()
+{
+  mpu1.getMotion6(&ax1, &ay1, &az1, &gx1, &gy1, &gz1);
+  mpu2.getMotion6(&ax2, &ay2, &az2, &gx2, &gy2, &gz2);
+
+	#if DEBUG
+		Serial.print(body_angle, 6);
+		Serial.print(",");
+		Serial.print(body_angle_dot, 6);
+		Serial.print(", ");
+		Serial.print(wheel_angle_dot, 6);
+    Serial.print(", ");
+    Serial.print(input_current, 6);
+		Serial.println("\r\n");
+	#endif
+}
+
+ISR(TIMER1_OVF_vect)
+{
+  digitalWrite(7, HIGH);
+  
+  // Get the calculated angle and angle dot dot
+//  body_angle = tilt_estimation(&ax1, &ax2, &ay1, &ay2, &body_angle);
+  body_angle = tilt_estimation(ax1, ax2, ay1, ay2);
+
+  body_angle_LPF = (1 - alpha_body_angle) * body_angle_LPF + alpha_body_angle * body_angle;
+   
+  // TODO: Add a Jacobian for frame transformation
+  // For right now assume mpu2 give a simular reading for angular velocity
+  body_angle_dot = (float)(gz2 * 133.0) / 100000.0;
+
+  wheel_angle_dot = maxon.getSpeedFeedback(countHz * 100);
+
+  input_current = Cube_Controller(body_angle,body_angle_dot,wheel_angle_dot);
+
+
+  // Set the maxon current every 20ms
+  maxon.setMotor(input_current);
+  // maxon.setMotor(20);
+    Serial.print(input_current);
+    Serial.print("\r\n");
+  TCNT1 = 0xB7A7;
+
+  digitalWrite(7, LOW);
+}
